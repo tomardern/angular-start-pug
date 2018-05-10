57728430b86d3a0dc8cb70adbdeac2645a6a9ddb
Tom Ardern <tomardern@bloomandwild.com>
2018-05-10 21:48:53 +0100

    added remote

--- scripts/testcafe-remote.js
+++ scripts/testcafe-remote.js
@@ -6,23 +6,58 @@ let testcafe = null;
 const jsonOutput = `./temp/${new Date().getTime()}.json`;
 const stream = fs.createWriteStream(jsonOutput);
 
-createTestCafe('localhost', 1340)
+var connectionPromises = [];
+
+/**
+ * Wait for 'x' amount to connect
+ * @param {testcafe} testcafe
+ * @param {SVGAnimatedInteger} remoteCount
+ */
+var waitForBrowsers = function (testcafe, remoteCount) {
+  var connectionPromises = [];
+
+  for (var i = 0; i < remoteCount; i++) {
+    const p = testcafe
+      .createBrowserConnection()
+      .then((bc) => {
+          return new Promise((resolve, reject) => {
+            bc.once('ready', () => {
+              console.log('CONNECTED', bc.userAgent);
+              resolve(bc);
+            });
+          });
+        })
+    connectionPromises.push(p);
+  }
+
+  return Promise.all(connectionPromises);
+}
+
+
+
+
+// testcafe remote:3 tests/sample-fixture.js
+
+
+createTestCafe('localhost', 1320)
   .then(tc => {
     testcafe = tc;
     runner = testcafe.createRunner();
+    console.log('[CONNECT URL]', testcafe.browserConnectionGateway.connectUrl);
 
-    return runner
-      .src('e2e/test-cafe/test1.js')
-      .browsers('chrome:headless')
-      .reporter('json', stream)
-      .reporter('spec')
-      .run();
+    return waitForBrowsers(testcafe, 1)
+      .then((browsers) => {
+        console.log('All connected...');
+        return runner
+          .src('e2e/test-cafe/test1.js')
+          .browsers(browsers)
+          //.concurrency(2)
+          .reporter('json', stream)
+          .reporter('spec')
+          .run();
+      });
   })
-  .then(failedCount => {
-    stream.end();
-
-    var data = fs.readFileSync(jsonOutput, 'utf8');
-    console.log(data);
-    testcafe.close();
-})
-.catch(e => console.error(e));
+  .then(d => {
+    console.log('d', d);
+  })
+  .catch(e => console.error(e));
