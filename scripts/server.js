e4a5ee2d9312411378b7dd4a5785118c74672b49
Tom Ardern <tomardern@bloomandwild.com>
2018-05-10 22:10:40 +0100

    Addex express server

--- scripts/server.js
+++ scripts/server.js
@@ -6,40 +6,71 @@ const fs = require('fs');
 
 const app = express();
 var concat = require('concat-stream');
-app.use(bodyParser.text({type: '*/*'}));
+app.use(bodyParser.text({ type: '*/*' }));
 
-let testcafe = null;
+
+/**
+ * Wait for 'x' amount to connect
+ * @param {testcafe} testcafe
+ * @param {SVGAnimatedInteger} remoteCount
+ */
+var waitForBrowsers = function (testcafe, remoteCount) {
+  var connectionPromises = [];
+
+  for (var i = 0; i < remoteCount; i++) {
+    const p = testcafe
+      .createBrowserConnection()
+      .then((bc) => {
+        return new Promise((resolve, reject) => {
+          bc.once('ready', () => {
+            console.log('CONNECTED', bc.userAgent);
+            resolve(bc);
+          });
+        });
+      })
+    connectionPromises.push(p);
+  }
+
+  return Promise.all(connectionPromises);
+}
+
 
 /**
  * Router
  */
 app.post('/', (req, res) => {
-  const testScript  = `./temp/script-${new Date().getTime()}.js`;
+  let testcafe = null;
+
+  const testScript = `./temp/script-${new Date().getTime()}.js`;
   fs.writeFileSync(testScript, req.body);
 
   const jsonOutput = `./temp/${new Date().getTime()}.json`;
   const stream = fs.createWriteStream(jsonOutput);
+  const portToUse = Math.floor(Math.random() * 9999) + 1000;
 
-  createTestCafe('localhost', 1340)
+  createTestCafe('localhost', portToUse)
     .then(tc => {
       testcafe = tc;
-      runner = testcafe.createRunner();
-
-      return runner
+      console.log('[CONNECT URL]', testcafe.browserConnectionGateway.connectUrl);
+      return waitForBrowsers(testcafe, 1)
+    })
+    .then((browsers) => {
+      console.log('All connected... running', testScript);
+      return testcafe.createRunner()
         .src(testScript)
-        .browsers('chrome:headless')
+        .browsers(browsers)
+        //.concurrency(2)
         .reporter('json', stream)
         .reporter('spec')
         .run();
     })
-    .then(failedCount => {
+    .then(() => {
       stream.end();
-
       var data = fs.readFileSync(jsonOutput, 'utf8');
       testcafe.close();
       res.status(200).send(data);
-  })
-  .catch(e => res.status(500).send(e.toString()));
+    })
+    .catch(e => res.status(500).send(e.toString()));
 });
 
 app.listen(3010, () => console.log('Example app listening on port 3010!'))
